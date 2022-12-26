daf65ac8557c052a49f64aa448c641cb61f7237a
rahulchauhan3j <r.rahul.c@gmail.com>
2022-12-26 16:09:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HardHat Lottery Project
+
+This project demonstrates a decentralized application for Lottery using Solidity and Javascript.
---
+++ hardhat.config.js
@@ -0,0 +1,50 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || " "
+const PRIVATE_KEY = process.env.PRIVATE_KEY || " "
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || " "
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || " "
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        goerli: {
+            chainId: 5,
+            blockConfirmations: 6,
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    solidity: "0.8.17",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gasReport.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
+    },
+    mocha: {
+        timeout: 800000,
+    },
+}
